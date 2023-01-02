a98ad1ec3200d0e4859923175751deee1c2f63e2
luckypeppa <871565198@qq.com>
2023-01-02 15:16:13 +0800

    Setup typography.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    Time to Review
+    <section>
+      <h2>计算机网络</h2>
+      <div>
+        <span>icon</span>
+        <h3>第一章</h3>
+        <div>1st review</div>
+      </div>
+    </section>
   </main>
 </template>
