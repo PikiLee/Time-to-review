e5845bccd1ef9b3029dbb0af22f20920288b9b28
luckypeppa <871565198@qq.com>
2023-01-02 17:11:46 +0800

    add(AppHeader)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import AppHeader from "./components/AppHeader.vue";
 </script>
 
 <template>
@@ -13,7 +14,8 @@ import { RouterView } from "vue-router";
     prose-warmgray
     dark:bg-warmgray-800
   >
-    <div w-3xl max-w-screen ma-auto pa-8 >
+    <AppHeader />
+    <div w-3xl max-w-screen ma-auto pa-8>
       <RouterView />
     </div>
   </div>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div grid grid-cols-6 py-2>
+    <div></div>
+    <div flex justify-start items-center flex-row gap-3 col-span-4>
+      <div>
+        <img
+          src="../assets/icon.png"
+          alt="website icon"
+          border-rounded-full
+          w-10
+        />
+      </div>
+      <h1 text-sm>Time To Review</h1>
+    </div>
+    <div></div>
+  </div>
+</template>
