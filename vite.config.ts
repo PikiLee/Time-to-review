738fe915e71ed93b47b0018252b14785d0ed6c70
luckypeppa <871565198@qq.com>
2023-01-02 14:36:22 +0800

    Set up unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,31 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// Unocss
+import Unocss from "unocss/vite";
+import presetAttributify from "@unocss/preset-attributify";
+import presetMini from "@unocss/preset-mini";
+import presetTypography from "@unocss/preset-typography";
+import presetUno from "@unocss/preset-uno";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Unocss({
+      presets: [
+        presetAttributify(),
+        presetMini(),
+        presetTypography(),
+        presetUno(),
+      ],
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
