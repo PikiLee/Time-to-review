2ca2e53d7d3e4cd3e08a20a344b2ec0130d1a80d
luckypeppa <871565198@qq.com>
2023-01-02 16:23:23 +0800

    Add(CoursesView)

---
+++ src/views/CoursesView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <h1>Course List</h1>
+    <ul list-none>
+      <li>
+        <h2>Progressing</h2>
+        <ul list-none grid grid-cols-2 gap-6 p-4>
+          <li
+            grid
+            grid-rows-2
+            grid-cols-4
+            gap-2
+            items-center
+            v-for="n in 5"
+            :key="n"
+          >
+            <div
+              row-span-2
+              i-ic-round-done
+              text-size-3xl
+              justify-self-center
+            ></div>
+            <h3 col-span-3 m-0>第一章</h3>
+            <div col-span-3>1st review</div>
+          </li>
+        </ul>
+      </li>
+      <li><h2>Done</h2></li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
