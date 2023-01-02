262a3d6716233ac1347ce925b2bf3551b3e6628d
luckypeppa <871565198@qq.com>
2023-01-02 16:35:46 +0800

    add(CourseView)

---
+++ src/views/CourseView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <h1>计算机网络原理</h1>
+    <ul list-none>
+      <li
+        grid
+        grid-cols-5
+        items-center
+        border-b
+        border-b-warmgray-300
+        pb-2
+        mb-2
+        v-for="n in 10"
+        :key="n"
+      >
+        <div>icon</div>
+        <h3 m-0>第一章</h3>
+        <span>1st Review</span>
+        <time>2022-2-2</time>
+        <time>2022-2-2</time>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
