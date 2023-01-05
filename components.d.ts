b1fad89605ff844f78cfd2729c2f485155c48f29
luckypeppa <871565198@qq.com>
2023-01-05 23:32:42 +0800

    refactor(CourseCreator)

--- components.d.ts
+++ components.d.ts
@@ -10,6 +10,7 @@ declare module '@vue/runtime-core' {
     AppHeader: typeof import('./src/components/AppHeader.vue')['default']
     Card: typeof import('./src/components/Course/Card.vue')['default']
     CourseCard: typeof import('./src/components/Course/CourseCard.vue')['default']
+    CourseCreator: typeof import('./src/components/Course/CourseCreator.vue')['default']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElLink: typeof import('element-plus/es')['ElLink']
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,35 +1,8 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { create } from "@/database/course";
-
-const input = ref("");
-const defaultPlaceholder = "New Course Name";
-const placeholder = ref(defaultPlaceholder);
-
-function handleCreate() {
-  try {
-    create(input.value);
-  } catch (error) {
-    placeholder.value = String(error);
-  }
-}
+import type CourseCreator from "./Course/CourseCreator.vue";
 </script>
 <template>
-  <div py-10 grid grid-cols-1 gap-2 auto-rows-min justify-items-center>
-    <el-input
-      v-model.trim="input"
-      :placeholder="placeholder"
-      @focus="placeholder = defaultPlaceholder"
-      @keyup.enter="handleCreate"
-      data-test="add-course-input"
-    />
-    <el-button
-      round
-      bg-lime-500
-      @click="handleCreate"
-      type="primary"
-      data-test="add-course"
-      >Create</el-button
-    >
+  <div>
+    <CourseCreator />
   </div>
 </template>
