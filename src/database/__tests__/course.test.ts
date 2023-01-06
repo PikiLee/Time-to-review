801a9b21bd92ff991c5faf412a109587a6f32d92
luckypeppa <871565198@qq.com>
2023-01-06 21:01:56 +0800

    add(course.test): del test

--- src/database/__tests__/course.test.ts
+++ src/database/__tests__/course.test.ts
@@ -1,6 +1,27 @@
-import { useCourses } from "./../course";
-import { describe, expect, test } from "vitest";
+import { del, useCourses } from "./../course";
+import { describe, expect, test, beforeEach } from "vitest";
 import { create } from "../course";
+import { CourseStatus } from "@/types/course.type";
+
+beforeEach(() => {
+  const courses = useCourses();
+  courses.value = [
+    {
+      id: 1,
+      name: "course1",
+      status: CourseStatus["In Progress"],
+      archived: false,
+      createdAt: Date.now(),
+    },
+    {
+      id: 2,
+      name: "course2",
+      status: CourseStatus["In Progress"],
+      archived: false,
+      createdAt: Date.now(),
+    },
+  ];
+});
 
 /**
  * Partition:
@@ -20,5 +41,24 @@ describe("Test add course", () => {
       (course) => course.name === courseName
     );
     expect(doesContain).toBe(true);
+  });
+});
+
+/**
+ * Partition:
+ *  course exists
+ *  course not exists
+ */
+describe("Test delete course", () => {
+  test("Cover course exists", () => {
+    const courses = useCourses();
+    del(1);
+    expect(courses.value.findIndex((course) => course.id === 1)).toBe(-1);
+  });
+
+  test("Cover course doesn't exist.", () => {
+    const courses = useCourses();
+    del(0);
+    expect(courses.value.length).toBe(2);
   });
 });
