2b4e3f7b947e330725caad1d7d646433e5f3da46
luckypeppa <871565198@qq.com>
2023-01-07 17:39:44 +0800

    change(ProgressCard): structure

--- src/components/Progress/ProgressCard.vue
+++ src/components/Progress/ProgressCard.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
 import {
   ProgressStageObjectReversed,
-  type Progress,
+  type ProgressStage,
 } from "@/types/progress.type";
 
 defineProps<{
-  progress: Progress;
+  name: string;
+  stage: ProgressStage;
 }>();
 </script>
 
 <template>
-  <div grid grid-rows-2 grid-cols-4 gap-2 items-center>
-    <div row-span-2 i-ic-round-done text-size-3xl justify-self-center></div>
-    <h3 col-span-3 m-0>{{ progress.name }}</h3>
-    <div col-span-3>
-      {{ ProgressStageObjectReversed[progress.stage] }}
+  <div grid grid-rows-2 grid-cols-1 gap-2 items-center>
+    <h3 m-0>{{ name }}</h3>
+    <div>
+      {{ ProgressStageObjectReversed[stage] }}
     </div>
   </div>
 </template>
