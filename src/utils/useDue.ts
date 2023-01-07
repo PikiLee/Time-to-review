1fa4437adabb358987329a61135dc43371fb1789
luckypeppa <871565198@qq.com>
2023-01-07 16:10:54 +0800

    refactor(useDue): now use rectify

--- src/utils/useDue.ts
+++ src/utils/useDue.ts
@@ -1,10 +1,10 @@
-import { computed, unref, type Ref } from "vue";
 import dayjs from "dayjs/esm";
+import { reactify } from "@vueuse/core";
 
-export function useDue(nextDate: Ref<string> | string) {
-  return computed(() => {
-    const next = dayjs(unref(nextDate)).valueOf();
-    const today = dayjs().startOf("day").valueOf();
-    return next - today <= 0;
-  });
+export function getIsDue(nextDate: string | number) {
+  const next = dayjs(nextDate).valueOf();
+  const today = dayjs().startOf("day").valueOf();
+  return next - today <= 0;
 }
+
+export const useDue = reactify(getIsDue);
