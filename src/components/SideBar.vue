28e9a74128d56b2d85c779d6591444ae18cf124b
luckypeppa <871565198@qq.com>
2023-01-07 18:06:35 +0800

    fix(SideBar)

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+import { computed } from "vue";
+import { useRoute } from "vue-router";
 import type CourseCreator from "./Course/CourseCreator.vue";
+
+const route = useRoute();
+const showCourseCreator = computed(() => route.name !== "home");
 </script>
 <template>
   <div mt-18>
-    <CourseCreator />
+    <CourseCreator v-if="showCourseCreator" />
   </div>
 </template>
