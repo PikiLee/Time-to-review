c6acde228f5e287587a533f890410e2853adb8b0
luckypeppa <871565198@qq.com>
2023-02-02 11:13:46 +0800

    test(frontend): add create progress test

--- packages/frontend/cypress/e2e/course.cy.ts
+++ packages/frontend/cypress/e2e/course.cy.ts
@@ -7,12 +7,33 @@ before(() => {
 
 describe("course", () => {
 	it("create course", () => {
+		// create course
+		cy.visit("/courses");
 		cy.get('[data-testid="add-button"]').click();
 		cy.get('[data-testid="course-creator"]').should("exist");
 
-		cy.get('[data-testid="course-creator"]').find("input").type("new course");
+		cy.get('[data-testid="course-creator"]')
+			.find("input")
+			.type(Math.random().toString());
 		cy.get('[data-testid="course-creator"]').find("button").click();
 
 		cy.get('[data-testid="course-card"]').should("have.length", 1);
+
+		//create progress
+		cy.get('[data-testid="course-card"]:first-of-type')
+			.find('[data-testid="course-card-title"]')
+			.click();
+
+		cy.url().should("contain", "/course");
+
+		cy.get('[data-testid="add-button"]').click();
+		cy.get('[data-testid="course-creator"]').should("exist");
+
+		cy.get('[data-testid="course-creator"]')
+			.find("input")
+			.type(Math.random().toString());
+		cy.get('[data-testid="course-creator"]').find("button").click();
+
+		cy.get('[data-testid="progress-list-item"]').should("have.length", 1);
 	});
 });
