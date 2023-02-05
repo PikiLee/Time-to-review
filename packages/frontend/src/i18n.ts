b5b7bc0d20ee6617383cbe207c566cac4d83548f
luckypeppa <871565198@qq.com>
2023-02-05 16:59:00 +0800

    feat(frontend): memorize language setting

--- packages/frontend/src/i18n.ts
+++ packages/frontend/src/i18n.ts
@@ -1,9 +1,24 @@
 import { createI18n } from 'vue-i18n'
 import { messages } from 'shared'
+import { usePreferredLanguages, useStorage } from '@vueuse/core'
+import { watchEffect } from 'vue'
+
+let locale: string
+const lang = useStorage('lang', '')
+if (lang) {
+	locale = lang.value
+} else {
+	locale = usePreferredLanguages().value[0]
+}
 
 export const i18n = createI18n({
 	legacy: false,
-	locale: 'en', // set locale
+	locale, // set locale
 	fallbackLocale: 'en', // set fallback locale
 	messages
 })
+
+watchEffect(() => {
+	lang.value = i18n.global.locale.value
+})
+
