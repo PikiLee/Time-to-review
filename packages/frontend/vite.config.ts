50ca33a3ce650ca883a2ba0a772e67f1864fd7fa
luckypeppa <871565198@qq.com>
2023-02-15 18:24:10 +0800

    fix(frontend): fix i18n

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,6 +1,5 @@
 /// <reference types="vitest" />
 import { fileURLToPath, URL } from 'node:url'
-import { resolve, dirname } from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -18,22 +17,10 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-import vueI18n from '@intlify/vite-plugin-vue-i18n'
-
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		vue(),
-		vueI18n({
-			// if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
-			// compositionOnly: false,
-
-			// you need to set i18n resource including paths !
-			include: resolve(
-				dirname(fileURLToPath(import.meta.url)),
-				'./path/to/src/locales/**'
-			)
-		}),
 		Unocss({
 			presets: [
 				presetAttributify(),
